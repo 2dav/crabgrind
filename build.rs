6f11cb902ef53e9e3a907bbd10c8f9fb1f29b845
Danila Davydov <2davy@pm.me>
2022-12-06 12:55:09 +0500

    Remove pkg-config since we don't need to link against libvalgrind

--- build.rs
+++ build.rs
@@ -1,11 +1,5 @@
 fn main() {
     println!("cargo:rerun-if-changed=export.c");
 
-    let mut cc = cc::Build::new();
-    if let Ok(lib) = pkg_config::Config::new().probe("valgrind") {
-        for include in &lib.include_paths {
-            cc.include(include);
-        }
-    }
-    cc.file("export.c").compile("libcrabgrind.a");
+    cc::Build::new().file("export.c").compile("libcrabgrind.a");
 }

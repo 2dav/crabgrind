3b458da400ade3add95b3c04be1c708cda5ade9e
2dav <2davy@pm.me>
2026-01-23 09:30:23 +0300

    Move 'mismatch-ignore' conditional switch to the same level with others

--- src/requests/mod.rs
+++ src/requests/mod.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(feature = "mismatch-ignore", allow(unused, unused_imports))]
 pub mod cachegrind;
 pub mod callgrind;
 pub mod dhat;
@@ -15,6 +16,7 @@ macro_rules! client_request {
         ($($arg as usize),*).0
     }};
     ($request:path, $arg1:expr, $arg2:expr, $arg3:expr, $arg4:expr, $arg5:expr) => {{
+        #[cfg(not(feature = "mismatch-ignore"))]
         $crate::requests::assert_defined!($request);
 
         client_request!(^ 0, $request, $arg1, $arg2, $arg3, $arg4, $arg5)
@@ -39,25 +41,22 @@ macro_rules! client_request {
 // Asserts that a client request is supported by the Valgrind version this crate was compiled against.
 // This assertion is expected to be optimized out by the compiler for supported requests.
 macro_rules! assert_defined {
-    ($request:path) => {
-        #[cfg(not(feature = "mismatch-ignore"))]
-        {
-            const REQUIREMENT: u32 = $request.required_version();
-            const SYS_VERSION: u32 = crate::VALGRIND_VERSION.0 * 100 + crate::VALGRIND_VERSION.1;
+    ($request:path) => {{
+        const REQUIREMENT: u32 = $request.required_version();
+        const SYS_VERSION: u32 = crate::VALGRIND_VERSION.0 * 100 + crate::VALGRIND_VERSION.1;
 
-            assert!(
-                REQUIREMENT <= SYS_VERSION,
-                "\n'{request}' is not supported by your Valgrind version. \n\
+        assert!(
+            REQUIREMENT <= SYS_VERSION,
+            "\n'{request}' is not supported by your Valgrind version. \n\
                 \t You are compiling against <valgrind/valgrind.h> {major}.{minor}. \n\
                 \t This request requires Valgrind {req_major}.{req_minor} or higher.\n",
-                request = &stringify!($request)[7..], // CR::CG_*
-                major = crate::VALGRIND_VERSION.0,
-                minor = crate::VALGRIND_VERSION.1,
-                req_major = REQUIREMENT / 100,
-                req_minor = REQUIREMENT % 100,
-            );
-        }
-    };
+            request = &stringify!($request)[7..], // CR::CG_*
+            major = crate::VALGRIND_VERSION.0,
+            minor = crate::VALGRIND_VERSION.1,
+            req_major = REQUIREMENT / 100,
+            req_minor = REQUIREMENT % 100,
+        );
+    }};
 }
 
 pub(crate) use {assert_defined, client_request};

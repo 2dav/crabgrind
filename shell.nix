4d35566074b4c47e91063a52f41d87b62f3571e8
2dav <2davy@pm.me>
2026-06-30 04:23:23 +0300

    Fix formatting, bump Valgrind to latest

--- shell.nix
+++ shell.nix
@@ -1,47 +1,55 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> {}}:
+
 let
-  overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
+  overrides = builtins.fromTOML (builtins.readFile ./rust-toolchain.toml);
+
   latestValgrind = pkgs.valgrind.overrideAttrs (old: rec {
-    version = "3.27.0";
+    version = "3.27.1";
 
     src = pkgs.fetchurl {
-      url = "https://www.sourceware.org/pub/valgrind/valgrind-${version}.tar.bz2";
-	  sha256 = "sha256-W1k33oJX7o9RaY6nG5cRrc6YBhqgfapKaF78OvkhW+8=";
+      url = "https://ftp.funet.fi/pub/mirrors/sourceware.org/pub/valgrind/valgrind-${version}.tar.bz2";
+      sha256 = "sha256-XViRUuuAccAv6rjOarcZ5DGh+8PisXAPVDJjKouSZNw=";
       # sha256 = pkgs.lib.fakeSha256;
     };
   });
-in 
-pkgs.callPackage ( { stdenv, mkShell }: mkShell {
-  strictDeps = true;
-  nativeBuildInputs = with pkgs; [
-	pkg-config
-    rustup
-    rustPlatform.bindgenHook
-	rust-analyzer
-    python3
+in
+pkgs.callPackage (
+  { stdenv, mkShell }:
 
-	cargo-cross
-	cspell
-	markdownlint-cli
-	just
+  mkShell {
+    strictDeps = true;
 
-	latestValgrind
-  ];
+    nativeBuildInputs = with pkgs; [
+      pkg-config
+      rustup
+      rustPlatform.bindgenHook
+      rust-analyzer
+      python3
 
-  buildInputs = with pkgs; [
-	latestValgrind.dev
-  ];
+      cargo-cross
+      cspell
+      markdownlint-cli
+      just
 
-  RUSTC_VERSION = overrides.toolchain.channel;
-  # VALGRIND_INCLUDE = "${latestValgrind.dev}/include";
+      latestValgrind
+    ];
 
-  shellHook = ''
-	export CARGO_HOME=''${CARGO_HOME:-$HOME/.cargo}
-	export RUSTUP_HOME=''${RUSTUP_HOME:-$HOME/.rustup}
-	export PATH=$PATH:$CARGO_HOME/bin
-    export PATH=$PATH:$RUSTUP_HOME/toolchains/$RUSTC_VERSION-${stdenv.hostPlatform.rust.rustcTarget}/bin
+    buildInputs = with pkgs; [
+      latestValgrind.dev
+    ];
 
-    # kinda fix for NixOS-specific ld-wrapper + Docker path resolution issues
-	export CROSS_CONTAINER_OPTS="-v $RUSTUP_HOME:$RUSTUP_HOME:z,ro";
-  '';
-}){}
+    RUSTC_VERSION = overrides.toolchain.channel;
+
+    # VALGRIND_INCLUDE = "${latestValgrind.dev}/include";
+
+    shellHook = ''
+      export CARGO_HOME=''${CARGO_HOME:-$HOME/.cargo}
+      export RUSTUP_HOME=''${RUSTUP_HOME:-$HOME/.rustup}
+      export PATH=$PATH:$CARGO_HOME/bin
+      export PATH=$PATH:$RUSTUP_HOME/toolchains/$RUSTC_VERSION-${stdenv.hostPlatform.rust.rustcTarget}/bin
+
+      # kinda fix for NixOS-specific ld-wrapper + Docker path resolution issues
+      export CROSS_CONTAINER_OPTS="-v $RUSTUP_HOME:$RUSTUP_HOME:z,ro"
+    '';
+  }
+){}
